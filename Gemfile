b43f024c5a1c8511640d4e720d54f3869814efd7
Joe Dowdell <joedowdell@gmail.com>
2014-08-18 16:41:15 +0100

    Linked to Paperclip linked to AWS

--- Gemfile
+++ Gemfile
@@ -47,3 +47,4 @@ gem 'capybara'
 end
 
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk'
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,5 +1,13 @@
 class Share < ActiveRecord::Base
+	has_attached_file :picture, 
+	styles: { medium: "300x300>" },
+	:storage => :s3,
+	:s3_credentials => {
+		:bucket => 'beardstagram',
+		:access_key_id => Rails.application.secrets.s3_access_key_id,
+		:secret_access_key => Rails.application.secrets.s3_secret_access_key_id
+	}
 
-  has_attached_file :picture, :styles => { :medium => "300x300>" }
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
